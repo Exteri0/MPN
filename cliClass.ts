60055329069962524ef3b0e70cde53efc3cfa564
Mohamed Sameh <mohamedsameh1574@gmail.com>
2024-09-05 10:59:10 -0400

    Added functionality to read the url file path
    the shell script now reads a path, checks if it is the url file path. then runs the cliclass and rightnow it just outputs the path again as an object attribute
    .

--- cliClass.ts
+++ cliClass.ts
@@ -6,14 +6,33 @@ import * as url from 'url';
 
 class CLI{
     private inputFilePath: string;
-    private inputURL: string;
-    private cliError: boolean;
+    private inputURL: string | null;
+    private cliError: boolean | null;
 
-   /*  public async findFile(): Promise<void>{
+    constructor(path: string) {
+        this.inputFilePath = path;
+        this.cliError = null;
+        this.inputURL = null;
+    }
+
+    public async printPath(): Promise<void>{
+        console.log(this.inputFilePath);
+    }
+
+/*     public async findFile(): Promise<void>{
         try {
             const fileStream;
         }
-    }
- */
+    } */
 
+};
+
+const argument = process.argv.slice(2);
+
+if (argument.length != 1) {
+    console.error('Invalid input');
+    process.exit(1);
 }
+
+let CLIObject = new CLI(argument[0]);
+CLIObject.printPath();
