125834cf69d370943e5a6d17fdf0b499838a52d0
MyronTadros <tadros.myron@gmail.com>
2024-09-22 08:44:46 -0400

    Ramp Up OOP

--- src/Metrics/RampUp.ts
+++ src/Metrics/RampUp.ts
@@ -2,95 +2,84 @@ import GitHubApiCalls from '../API/GitHubApiCalls.js';
 import NpmApiCalls from '../API/NpmApiCalls.js';
 import ApiCalls from '../API/api.js';
 
-export default class RampUpTime {
-    static calculateRampUpTimeGithub(data: any): number {
-        const { stargazers_count, forks_count, open_issues_count, watchers_count, has_wiki, has_pages, has_discussions } = data
+export class RampUpTime {
+    private apiCall: GitHubApiCalls | NpmApiCalls;
 
-        // Used log normalization as the distribution of the values is extreme therefore any other normalization will not be accurate for numbers
-        // that are small but good.
-        const normalizedStargazers = Math.log(stargazers_count + 1) / Math.log(10000 + 1); 
-        const normalizedForks = Math.log(forks_count + 1) / Math.log(10000 + 1);           
-        const normalizedOpenIssues = 1 - Math.log(open_issues_count + 1) / Math.log(1000 + 1); // The more the issues the worse the ramp-up.
-        const normalizedWatchers = Math.log(watchers_count + 1) / Math.log(3000 + 1);
-
-        const wikiScore = has_wiki ? 1 : 0;
-        const pagesScore = has_pages ? 1 : 0;
-        const discussionsScore = has_discussions ? 1 : 0;
-
-        const weightStargazers = 0.15;
-        const weightForks = 0.15;
-        const weightOpenIssues = 0.15;
-        const weightWatchers = 0.05;
-        const weightWiki = 0.25;
-        const weightPages = 0.15;
-        const weightDiscussions = 0.1;
-
-
-        const rampUpScore = (normalizedStargazers * weightStargazers) + (normalizedForks * weightForks) + (normalizedOpenIssues * weightOpenIssues) +
-                            (normalizedWatchers * weightWatchers) + (wikiScore * weightWiki) + (pagesScore * weightPages) + (discussionsScore * weightDiscussions);
-
-        return rampUpScore
+    constructor(apiCall: GitHubApiCalls | NpmApiCalls) {
+        this.apiCall = apiCall;
     }
 
-    static calculateRampUpTimeNPM(data: any): number {
-        const { readme, Versions, Maintainers, Dependencies, repository, githubrepository } = data
 
-        const normalizedVersions = Math.log(Versions+1) / Math.log(50+1); 
+    public async computeRampUpTime(): Promise<number> {
+        const response = await this.apiCall.handleAPI();
 
-        const normalizedMaintainers = Math.log(Maintainers+1) / Math.log(10+1);
-
-        const normalizedDependencies = 1 - (Math.log(Dependencies+1) / Math.log(20+1));
-
-        const weightReadme = 0.25;
-        const weightVersions = 0.20;
-        const weightMaintainers = 0.15;
-        const weightDependencies = 0.25;
-        const weightGithubRepository = 0.15;
-
-        const rampUpScore = (readme * weightReadme) +
-                            (normalizedVersions * weightVersions) +
-                            (normalizedMaintainers * weightMaintainers) +
-                            (normalizedDependencies * weightDependencies) +
-                            (githubrepository * weightGithubRepository);
+        if (!response) return -1;  
 
 
-        return rampUpScore
+        let rampUpScore = 0;
+
+        // The approach taken to calculate rampscore is a datascience based approach where the variables are normalized using log normalization
+        // then multiplied by the weight of each variable respectively to get the final score
+        
+        if (this.apiCall instanceof GitHubApiCalls) { // We use nullish coalescing to check if certain variables might return undefined or null
+            const normalizedStargazers = await this.normalizeLog(response.stargazers_count, 10000);
+            const normalizedForks = await this.normalizeLog(response.forks_count, 10000);
+            const normalizedOpenIssues = 1 - (await this.normalizeLog((response.open_issues_count ?? 0), 1000)); // Open issues could be undefined or null
+            const normalizedWatchers = await this.normalizeLog((response.watchers_count ?? 0), 3000); // Normalized watchers could be undefined or null
+            const wikiScore = response.has_wiki ? 1 : 0;
+            const pagesScore = response.has_pages ? 1 : 0;
+            const discussionsScore = response.has_discussions ? 1 : 0;
+
+            const weightStargazers = 0.15;
+            const weightForks = 0.15;
+            const weightOpenIssues = 0.15;
+            const weightWatchers = 0.05;
+            const weightWiki = 0.25;
+            const weightPages = 0.15;
+            const weightDiscussions = 0.1;
+
+            rampUpScore = (normalizedStargazers * weightStargazers) + (normalizedForks * weightForks) + (normalizedOpenIssues * weightOpenIssues) +
+                            (normalizedWatchers * weightWatchers) + (wikiScore * weightWiki) + (pagesScore * weightPages) + (discussionsScore * weightDiscussions);
+
+        } else if (this.apiCall instanceof NpmApiCalls) { // We use optional chaining because we are trying to find length of arrays which could throw an error
+            const readmeScore = response.readme ? 1 : 0;
+            const normalizedVersions = await this.normalizeLog((response.versions?.length ?? 0), 50);
+            const normalizedMaintainers = await this.normalizeLog((response.maintainers?.length ?? 0), 10);
+            const normalizedDependencies = 1 - (await this.normalizeLog((response.dependencies?.length ?? 0), 20));
+            const githubRepositoryScore = (typeof response.repository === 'object' && response.repository?.url?.includes("github")) ? 1 : 0;
+
+
+            const weightReadme = 0.25;
+            const weightVersions = 0.20;
+            const weightMaintainers = 0.15;
+            const weightDependencies = 0.25;
+            const weightGithubRepository = 0.15;
+
+            rampUpScore = (readmeScore * weightReadme) + (normalizedVersions * weightVersions) + (normalizedMaintainers * weightMaintainers) +
+                            (normalizedDependencies * weightDependencies) + (githubRepositoryScore * weightGithubRepository);
+
+        }
+
+        return rampUpScore;
+    }
+
+    
+    // Log normalization function
+    private async normalizeLog(value: number, maxValue: number): Promise<number> {
+        return Math.log(value + 1) / Math.log(maxValue + 1);
     }
 }
 
 
 (async () => {
-    const apiInstance = new ApiCalls(["https://github.com/nullivex/nodist"]) // Hardcopy remove it
-    const APIObj = await apiInstance.callAPI()
-    let RampUpCalculator: RampUpTime
-    let score: number
-    if (APIObj instanceof GitHubApiCalls) {
-        const response = await APIObj.handleAPI();
+    const apiInstance = new ApiCalls(["https://www.npmjs.com/package/express"]); 
+    const APIObj = await apiInstance.callAPI();
+    let score = -1;
 
-        score = await RampUpTime.calculateRampUpTimeGithub({
-            stargazers_count: response.stargazers_count,
-            forks_count: response.forks_count,
-            open_issues_count: response.open_issues_count,
-            watchers_count: response.watchers_count,
-            has_wiki: response.has_wiki,
-            has_pages: response.has_pages,
-        });
-
-        console.log(score)
+    if (APIObj instanceof NpmApiCalls || APIObj instanceof GitHubApiCalls){
+        let rampUpCalculator = new RampUpTime(APIObj);
+        const score = await rampUpCalculator.computeRampUpTime();
     }
-    else if (APIObj instanceof NpmApiCalls) {
-        const response = await APIObj.handleAPI();
-
-        score = await RampUpTime.calculateRampUpTimeNPM({
-            readme : response.readme ? 1:0,
-            Versions: Object.keys(response.versions).length,
-            Maintainers: response.maintainers.length,
-            Dependencies: Object.keys(response.dependencies).length,
-            repository: response.repository,
-            githubrepository: response.repository.url.includes("github") ? 1:0,
-        });
-
-        console.log(score)
-
-    }
+    
+    console.log('Ramp-up time score:', score);
 })();
